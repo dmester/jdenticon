9da25272a899d7d15363f6c002f8f14d412a5bf8
dmester <daniel@mester.se>
2020-07-23 09:32:59 +0200

    Fixed IE9 issues in browser tests.

--- test/browser/common.js
+++ test/browser/common.js
@@ -1,19 +1,22 @@
 
 addEventListener("message", function (ev) {
-    if ("scrollHeight" in ev.data) {
-        var iframe = document.getElementsByName(ev.data.name);
+    var data = JSON.parse(ev.data);
+    
+    if ("scrollHeight" in data) {
+        var iframe = document.getElementsByName(data.name);
         if (iframe && iframe.length) {
-            iframe[0].style.height = ev.data.scrollHeight + "px";
+            iframe[0].style.height = data.scrollHeight + "px";
         }
     }
 });
 
 function postHeight(timeout) {
     setTimeout(function () {
-        window.parent.postMessage({
+        // IE9 does not support passing objects through postMessage
+        window.parent.postMessage(JSON.stringify({
             scrollHeight: document.body.scrollHeight,
             name: window.name
-        }, "*");
+        }), "*");
     }, timeout);
 }
 
